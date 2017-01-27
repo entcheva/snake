3a9ea48c81643a344d66737b6fa976699f672c42
entcheva <christina.entcheva@gmail.com>
2017-01-27 10:02:43 -0500

    Replace DOMContentLoaded with window.onload

--- javascripts/board.js
+++ javascripts/board.js
@@ -1,28 +1,43 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+// document.addEventListener("DOMContentLoaded", function(event) {
 
-  var canvas = document.getElementById("myCanvas")
-  var ctx = canvas.getContext("2d")
+window.onload = function() {
 
+  // setup
+  canvas = document.getElementById("myCanvas")
+  ctx = canvas.getContext("2d")
+  speed = 5
+  positionX = 10
+
+
+
+  // create food
   function makeFood(x, y, xs, ys, color) {
     ctx.fillStyle = color
     ctx.fillRect(x, y, xs, ys) // x, y, sizex, size y
   }
 
-  function makeSnakeUnit(x, y, xs, ys, color) {
+
+  // create snake
+  function snakeUnit(x, y, xs, ys, color) {
     ctx.fillStyle = color
     ctx.fillRect(x, y, xs, ys) // x, y, sizex, size y
   }
 
-  function growSnake(x, y, xs, ys, color) {
-    // add 1 makeSnakeUnit to existing makeSnakeUnit
-    ctx.fillStyle = color
-    // conditional: depending on direction of movement
-    // append to last snakeUnit + 20
-    ctx.fillRect(x, y, xs, ys) // x, y, sizex, size y
-
+  // move snake
+  function move(piece) {
+    position += speed
   }
 
 
+  // // add to snake length
+  // function growSnake(x, y, xs, ys, color) {
+  //   // add 1 snakeUnit to existing snakeUnit
+  //   ctx.fillStyle = color
+  //   // conditional: depending on direction of movement
+  //   // append to last snakeUnit + 20
+  //   ctx.fillRect(x, y, xs, ys) // x, y, sizex, size y
+  //
+  // }
 
 
 
@@ -32,11 +47,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 
+
+  // call functions
 
   makeFood(90, 60, 20, 20, 'blue')
-  makeSnakeUnit(10, 10, 20, 20, 'green')
-  growSnake(30, 10, 20, 20, 'green')
-  growSnake(50, 10, 20, 20, 'green')
+  snakeUnit(positionX, 10, 20, 20, 'green')
+  // growSnake(30, 10, 20, 20, 'green')
+  // growSnake(50, 10, 20, 20, 'green')
+  // move()
 
 
-})
+
+
+
+
+
+
+
+}
+
+
+// end of document load
+// }) // end of document load
